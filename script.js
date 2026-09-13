514014b8f23e7d03baaa38cd6109e2bacd7cd64e
msarika07 <sarikamatamala@gmail.com>
2026-09-13 16:37:51 +0530

    Refactor analyzeRisk function for cleaner code

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function analyzeRisk() {
         return;
     }
 
-    fetch("http://127.0.0.1:5000/predict", {
+    fetch("/predict", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
